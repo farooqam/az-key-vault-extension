e55cc28cdbcaf1d5d7c9cec28a6ff0c67afd8f90
Farooq Mahmud <farooq_am@hotmail.com>
2019-08-05 12:12:13 -0700

    wip

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Api
 {
@@ -12,6 +16,14 @@ namespace Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var config = builder.Build();
+                    var tokenProvider = new AzureServiceTokenProvider();
+
+                    var kvClient = new KeyVaultClient((authority, resource, scope) => tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+                    builder.AddAzureKeyVault(config["KeyVault"], kvClient, new DefaultKeyVaultSecretManager());
+                })
                 .UseStartup<Startup>();
     }
 }
