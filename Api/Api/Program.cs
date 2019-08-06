42a5f31866c97094224b6d17c35345973947adb0
farooqam <farooq_am@hotmail.com>
2019-08-06 01:35:10 -0700

    Get connection strings from key vault (#6)

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Logging;
 
 namespace Api
 {
@@ -12,6 +17,19 @@ namespace Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var config = builder.Build();
+                    var tokenProvider = new AzureServiceTokenProvider();
+
+                    var kvClient = new KeyVaultClient((authority, resource, scope) => tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+                    builder.AddAzureKeyVault(config["KeyVault"], kvClient, new DefaultKeyVaultSecretManager());
+                })
+                .ConfigureLogging((ctx, logging) =>
+                {
+                    logging.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+                    logging.AddEventSourceLogger();
+                })
                 .UseStartup<Startup>();
     }
 }
